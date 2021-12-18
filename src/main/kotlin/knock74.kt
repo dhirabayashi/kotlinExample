44041717a28d11a0a15dc9a79c061b6ebac97c6e
daiki <gigabowser@hotmail.co.jp>
2021-12-18 11:28:01 +0900

    解答追加

--- src/main/kotlin/knock74.kt
+++ src/main/kotlin/knock74.kt
@@ -1,10 +1,11 @@
+import java.awt.Color
 import java.awt.Graphics
 import javax.swing.JComponent
 import javax.swing.JFrame
 import javax.swing.SwingUtilities
 import javax.swing.Timer
 
-class BoundDouble: JFrame() {
+class BoundDoubleWithColor: JFrame() {
     private val windowWidth = 600
     private val windowHeight = 400
 
@@ -15,26 +16,17 @@ class BoundDouble: JFrame() {
         defaultCloseOperation = EXIT_ON_CLOSE
 
         val mainPanel = contentPane
-        mainPanel.add(BoundDoubleComponent(windowWidth, windowHeight))
+        mainPanel.add(BoundDoubleComponentWithColor(windowWidth, windowHeight))
     }
 }
 
-data class BoundCircle(
-    var x: Int,
-    var y: Int,
-    var incrementalX: Int,
-    var incrementalY: Int
-)
-
-class BoundDoubleComponent(private val windowWidth: Int, private val windowHeight: Int): JComponent() {
+class BoundDoubleComponentWithColor(private val windowWidth: Int, private val windowHeight: Int): JComponent() {
     private val radius = 30
     private val size = radius * 2
-    private val initX = 30
-    private val initY = windowHeight - radius
 
     private val circles = listOf(
-        BoundCircle(initX, initY, 5, -3),
-        BoundCircle(initX, initY, 3, -5)
+        BoundCircle(30, windowHeight - 30, 5, -3),
+        BoundCircle(570, windowHeight - 370, -3, 5)
     )
     private val delay = 50
     private var timer: Timer = Timer(delay) {
@@ -70,15 +62,34 @@ class BoundDoubleComponent(private val windowWidth: Int, private val windowHeigh
     }
 
     override fun paintComponent(g: Graphics) {
+        // 色の判定
+        val x1 = circles[0].x - radius
+        val x2 = circles[0].x + radius
+        val x3 = circles[1].x - radius
+        val x4 = circles[1].x + radius
+
+        val y1 = circles[0].y - radius
+        val y2 = circles[0].y + radius
+        val y3 = circles[1].y - radius
+        val y4 = circles[1].y + radius
+
+        if((x1 in x3..x4 || x2 in x3..x4) &&
+            (y1 in y3..y4 || y2 in y3..y4)) {
+            g.color = Color.RED
+        } else {
+            g.color = Color.BLUE
+        }
+
         // 円の描画
         circles.forEach { circle ->
             g.drawOval(circle.x - radius, circle.y - radius, size, size)
+            g.fillOval(circle.x - radius, circle.y - radius, size, size)
         }
     }
 }
 
 fun main() {
     SwingUtilities.invokeLater {
-        BoundDouble().isVisible = true
+        BoundDoubleWithColor().isVisible = true
     }
 }
